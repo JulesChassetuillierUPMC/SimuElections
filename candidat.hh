2319b7a36c12ceaadfd7ba172b73d57f368dae5c
LEGHERABA Mohamed <3300273@pc4024.polytech.upmc.fr>
2016-12-06 11:58:11 +0100

    Test

--- candidat.hh
+++ candidat.hh
@@ -1,16 +1,17 @@
 #pragma once
-#include <stdlib>
-#include <iostream>
+#include "media.hh"
 
 class Candidat
 {
 	public: 
-		Candidat(std::string p, int image); // constructeur 
+		Candidat(std::string name,std::string p, int image); // constructeur 
 		~Candidat(); // destructeur
 		std::string get_personnalite();
 		int get_image();
-
+		void interview(Media M); // modifie l'image du candidat
+		std::string get_nom();
 	private:
+		std::string _nom;
 		std::string _personnalite; // neutre, gentil, gros enculé
 		int _image; // [0 3] : mauvaise, [4 7] : moyenne, [8 10] : bonne 	
 };
