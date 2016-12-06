1d6c839446d22f748cb7bafab9e1583dcd130b9e
CHASSETUILLIER Jules <3202757@pc4025.polytech.upmc.fr>
2016-12-06 11:19:40 +0100

    Methodes marchent, celles qui influencent les attributs de candidat et electeur restent naives

--- media.hh
+++ media.hh
@@ -1,15 +1,16 @@
 #pragma once
-#include <stdlib>
 #include <iostream>
 
-class media
+class Media
 {
 	public:
-		media(std::string name, int influence); // constructeur
-		~media(); // destructeur
+		Media(std::string name, int influence); // constructeur
+		~Media(); // destructeur
+		std::string get_name();
+		int get_influence();
 
 	private:
 		std::string _nom;
-		int _influence;
+		int _influence; // faible[0 3], moyenne [4 7], forte [8 10]
 
 };
