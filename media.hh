92b1f96544bd564b0b9114768f748d3d61dc598b
CHASSETUILLIER Jules <3202757@pc4025.polytech.upmc.fr>
2016-12-06 09:43:11 +0100

    bons fichiers cette fois

--- media.hh
+++ media.hh
@@ -1,3 +1,15 @@
 #pragma once
 #include <stdlib>
 #include <iostream>
+
+class media
+{
+	public:
+		media(std::string name, int influence); // constructeur
+		~media(); // destructeur
+
+	private:
+		std::string _nom;
+		int _influence;
+
+};
